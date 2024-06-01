770e4d8143786a7f995503ce82c1137fc96b7827
Maciek <maciejsdev@gmail.com>
2024-06-01 13:58:43 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import data from "./data";
-import Questions from "./components/questions";
+import Questions from "./components/Questions";
 
 const App = () => {
   const [questions, setQuestions] = useState(data);
