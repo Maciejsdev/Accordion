a9c43f3fc4565c8ff8db6fe90d62932a9bc99dbd
Maciek <maciejsdev@gmail.com>
2024-06-01 13:48:51 +0200

    Better toggle

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,12 +1,17 @@
 import SingleQuestion from "./SingleQuestion";
 
-const Questions = ({ questions }) => {
+const Questions = ({ questions, activeId, toggleQuestion }) => {
   return (
     <section className="container">
       <h2>Questions</h2>
       {questions.map((question) => {
         return (
-          <SingleQuestion key={question.id} {...question}></SingleQuestion>
+          <SingleQuestion
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          ></SingleQuestion>
         );
       })}
     </section>
